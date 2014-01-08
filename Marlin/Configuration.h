a6aec970f5024733c441cf4b7d6edcc6241c9566
whosawhatsis <whosawhatsis@gmail.com>
2014-01-07 17:18:56 -0800

    Add option for new SMD bed thermistor

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -3,6 +3,7 @@
 
 #define SPITFIRE
 #define DRV8825
+#define SMD_BED_THERMISTOR
 //#define TATSU_MINI
 //#define VIKI                // only successfully tested with Arduino 1.0.4
 //#define DIRECT_DRIVE_EXTRUDER
@@ -142,7 +143,11 @@
 #define TEMP_SENSOR_0 7
 #define TEMP_SENSOR_1 7
 #define TEMP_SENSOR_2 0
-#define TEMP_SENSOR_BED 1
+#ifdef SMD_BED_THERMISTOR
+  #define TEMP_SENSOR_BED 8
+#else
+  #define TEMP_SENSOR_BED 1
+#endif
 
 // This makes temp sensor 1 a redundant sensor for sensor 0. If the temperatures difference between these sensors is to high the print will be aborted.
 //#define TEMP_SENSOR_1_AS_REDUNDANT
